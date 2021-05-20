f42836a5ffdf20949cb1a6d841ec3084d7a62178
Ariel <arielbrugi20@gmail.com>
2021-05-20 17:29:49 -0300

    Primer Comit

---
+++ src/main/java/com/example/itbaftest/ItbaftestApplication.java
@@ -0,0 +1,13 @@
+package com.example.itbaftest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItbaftestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItbaftestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/itbaftest/ItbaftestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itbaftest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItbaftestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
